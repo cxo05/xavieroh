d9369c442399e2b0afbc88e3fb80f3c5080e523e
cxo05 <xavieroh05@gmail.com>
2022-07-22 16:45:26 +0800

    Add manifest

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
